c72ceb8ce649aad419da52961ff6c6a688bf82ff
Pooja Kancherla <pknq6@mst.edu>
2018-04-28 18:09:46 -0500

    Used if-else istead of try-catch

--- DictionariesAndMaps.py
+++ DictionariesAndMaps.py
@@ -3,10 +3,8 @@ n = int(input())
 d = dict(input().split() for _ in range(n))
 
 for i in range(n):
-
-    try:
-        query = input()
+    query = input()
+    if query in d.keys():
         print(query + '=' + d[query])
-
-    except:
+    else:
         print("Not found")
