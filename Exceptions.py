7a7b9fb261a6db4a973d650d8bd2078a4729cc5b
Pooja Kancherla <pknq6@mst.edu>
2018-08-24 22:36:05 -0500

    To raise an exception when either of the inputs are negative

---
+++ Exceptions.py
@@ -0,0 +1,20 @@
+class Calculator(object):
+    def power(self, n, p):
+
+        product = 1
+        if n < 0 or p < 0:
+            raise Exception( "n and p should be non-negative")
+
+        for i in range(p):
+            product *= n
+        return product
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)
