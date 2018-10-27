4f9b4a8487a08961430a54e317528f6d34728901
poojakancherla <pknq6@mst.edu>
2018-10-26 22:37:57 -0500

    Final Edits

--- BinarySearchTrees.py
+++ BinarySearchTrees.py
@@ -8,19 +8,9 @@ class Solution:
             return Node(data)
         else:
             if data<=root.data:
-            #     if root.left is None:
-            #         root.left = root.data
-            #     else:
-            #         self.insert(root.left, root.data)
                 root.left=self.insert(root.left,data)
-                # root.left=cur
             else:
-                # if root.right is None:
-                #     root.right = root.data
-                # else:
-                #     self.insert(root.right, data)
                 root.right=self.insert(root.right,data)
-                # root.right=cur
         return root
 
     def getHeight(self,root):
